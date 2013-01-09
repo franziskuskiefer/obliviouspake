45a21e26e053bf721d028a36ebe184c0a5d52b5b
Franziskus Kiefer <fk00042@csd032.eps.surrey.ac.uk>
2013-01-09 09:44:45 +0000

    new structure

--- OSPAKE-Test/build.sh
+++ OSPAKE-Test/build.sh
@@ -1,3 +1,9 @@
+EXTRA_PATH="-I ~/lib/Botan-1.10.3/build/include/ -L ~/lib/Botan-1.10.3/"
+#if [[ ":$LD_LIBRARY_PATH:" != *":/user/cspgr/fk00042/lib/Botan-1.10.3/:"* ]]; then
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/user/cspgr/fk00042/lib/Botan-1.10.3/
+	export LD_LIBRARY_PATH
+#fi
+
 if [ $# -eq 0 ] 
 then
 	echo "Usage: $0 [client,server,dh,ospake,spake]"
@@ -13,7 +19,8 @@ then
 	g++ DiffieHellman.cpp -pthread -lboost_system -lbotan-1.10 -o dh
 elif [ $1 = 'ospake' ] 
 then
-	g++ oSpake.cpp IHME.c -pthread -lboost_system -lbotan-1.10 -lgnutls -lgcrypt -o ospake
+#	g++  oSpake.cpp IHME.c -pthread -lboost_system -lbotan-1.10 -lgnutls -lgcrypt -o ospake
+	g++ -I ~/lib/Botan-1.10.3/build/include/ -L ~/lib/Botan-1.10.3/ oSpake.cpp IHME.c -pthread -lboost_system -lbotan-1.10 -lgnutls -lgcrypt -o ospake
 elif [ $1 = 'spake' ] 
 then
 	g++ Spake.cpp -pthread -lboost_system -lbotan-1.10 -o spake
